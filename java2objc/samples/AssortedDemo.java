a15d7107444730ab77e4d64dbe807ce22a48ef49
inder123 <inder123@f07ea3ba-6614-11de-b47b-210fc227597e>
2009-07-10 17:50:12 +0000

    Made Sample1 a demo of assorted items.

---
+++ java2objc/samples/AssortedDemo.java
@@ -0,0 +1,31 @@
+/**
+ * Demo of assorted language constructs
+ * 
+ * @author Inderjeet Singh
+ */
+public class AssortedDemo {
+
+  public int labeledStatementDemo() {
+    int value = 4;
+
+    outer:
+    for (int i = 0; i < value; ++i) {
+      for (int j = 0; j < value; ++j) {
+        if (i*j == 12) {
+          break outer;
+        } else {
+          continue;
+        }
+      }
+    }
+    return value;
+  }
+
+  public String getAnotherValue(String value) {
+    return "Value is: " + value;
+  }
+  
+  public String methodDemo() {
+    return getAnotherValue("foo");
+  }
+}
