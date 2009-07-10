cd5170a480c3c3040e50f19dfa85feb780855034
inder123 <inder123@f07ea3ba-6614-11de-b47b-210fc227597e>
2009-07-10 20:31:22 +0000

    renamed samples to examples, placed them in an examples package, and renamed demo suffixes to examples. Hooked in method call expression translation as well.

---
+++ trunk/java2objc/examples/examples/InitExamples.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2009 Inderjeet Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package examples;
+
+/**
+ * Examples of how java2objc converts Java constructors into Objective C init methods
+ * 
+ * @author Inderjeet Singh
+ */
+@SuppressWarnings("unused")
+public class InitExamples {
+  private final int a;
+  private final String b;
+  private final String c;
+  private final double d;
+  
+  public InitExamples(int a, String b) {
+    this(a, b, "c", 2.0);
+  }
+
+  public InitExamples(int a, String b, String c, double d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+  }
+}
---
+++ trunk/java2objc/examples/examples/MethodExamples.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2009 Inderjeet Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package examples;
+
+/**
+ * Examples of how java2objc converts Java static and object method invocations into their 
+ * Objective C equivalents
+ * 
+ * @author Inderjeet Singh
+ */
+public class MethodExamples {
+
+  public static int staticMethod(int a, int b) {
+    return a+b;
+  }
+
+  public static int callAStaticMethod(int a, int b) {
+    return staticMethod(a, b);
+  }
+
+  public String method1(String value) {
+    return "Value is: " + value;
+  }
+  
+  public String callAMethod() {
+    return method1("foo");
+  }
+}
