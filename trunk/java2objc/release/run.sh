a966679bd8976481767ad953e37ecc16c18fb639
gileadis <gileadis@f07ea3ba-6614-11de-b47b-210fc227597e>
2010-11-24 13:33:04 +0000

    Adding a release folder.

---
+++ trunk/java2objc/release/run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#setup classpath
+CP="./java2objc.jar"
+for i in `ls ./lib/*.jar`
+do
+  CP=${CP}:${i}
+done
+
+java -Xmx1g -cp "${CP}" com.googlecode.java2objc.main.Main --outputdir=output $*
+
+exit 0
